fde8b9d860908837848cbf83921c6593f627eda0
Andy Castillo <andy.castillo@sturgiswebservices.com>
2014-03-20 12:01:00 -0400

    Add barcode scanner

--- js/angularApp.js
+++ js/angularApp.js
@@ -9,5 +9,19 @@ angular.module('Kiosk', [])
             });
     }
 
+    $scope.scanQR = function () {
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
+            },
+            function (error) {
+                alert("Scanning failed: " + error);
+            }
+        );
+    }
+
 } ]);
 
