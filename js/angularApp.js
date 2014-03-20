7198de08ff103a0abbe10f308ac9e5e36385c75b
Andy Castillo <andy.castillo@sturgiswebservices.com>
2014-03-20 12:33:34 -0400

    fix?

--- js/angularApp.js
+++ js/angularApp.js
@@ -10,21 +10,38 @@ angular.module('Kiosk', [])
     }
 
     $scope.scanQR = function () {
-        alert("scan!");
-        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+        console.log("scanning");
+        try {
+            var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+            console.log("Scanner: " + scanner); k
 
-        alert(scanner);
-        scanner.scan(
-            function (result) {
+            scanner.scan(function (result) {
+
                 alert("We got a barcode\n" +
-                    "Result: " + result.text + "\n" +
-                    "Format: " + result.format + "\n" +
-                    "Cancelled: " + result.cancelled);
-            },
-            function (error) {
-                alert("Scanning failed: " + error);
-            }
-        );
+            "Result: " + result.text + "\n" +
+            "Format: " + result.format + "\n" +
+            "Cancelled: " + result.cancelled);
+
+                console.log("Scanner result: \n" +
+                "text: " + result.text + "\n" +
+                "format: " + result.format + "\n" +
+                "cancelled: " + result.cancelled + "\n");
+                document.getElementById("info").innerHTML = result.text;
+                console.log(result);
+                /*
+                if (args.format == "QR_CODE") {
+                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+                }
+                */
+
+            }, function (error) {
+                console.log("Scanning failed: ", error);
+            });
+        }
+        catch (ex) {
+            console.log("error: " + ex);
+        }
+
     }
 
 } ]);
